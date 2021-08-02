1f066d33d7fe505c89111f2c7223a223e2cea0dc
x1unix <denis0051@gmail.com>
2021-08-02 17:30:03 +0300

    Move text generator to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::result::Result;
 use std::env;
 use std::fs;
 use std::process;
+use lorem::{generate, parse_expression, Expression};
 
-struct Expression (usize, String);
+mod lorem;
 
 fn main() {
     match env::args().len() {
@@ -17,11 +17,8 @@ fn main() {
                 _ => {}
             }
             match parse_expression(&argument) {
-                Ok(v) => {
-                    let Expression(count, delimiter) = v;
-                    lorem(count, &delimiter);
-                },
-                Err(err) => return println!("Error: {}", err)
+                Ok(v) => print_lorem(v),
+                Err(err) => println!("Error: {}", err)
             };
         },
         _ => {
@@ -31,49 +28,12 @@ fn main() {
     }
 }
 
-fn parse_expression(expression: &str) -> Result<Expression, &str> {
-    let input = expression.trim();
-    if input.len() < 2 {
-        return Err("Empty string");
-    }
 
-    let count: usize = match input[0..input.len()-1].parse() {
-        Ok(n) => n,
-        Err(_) => return Err("Invalid count")
-    };
-    let suffix = match input.chars().last() {
-        Some(s) => s,
-        None => return Err("Missing argument suffix")
-    };
-    let delimiter: &str = match suffix {
-        'w' => " ",
-        's' => ". ",
-        'c' => "\0",
-        _ => return Err("Invalid argument suffix")
-    };
-
-    Ok(Expression(count, String::from(delimiter)))
-}
-
-fn lorem(count: usize, delimiter: &str) {
+fn print_lorem(expr: Expression) {
     let text = fs::read_to_string("lorem.txt").unwrap();
-    let mut read_count = count;
-    let chunks: String = match delimiter {
-        "\0" => {
-            if text.len() < read_count {
-                read_count = text.len();
-            }
-            String::from(&text[..read_count])
-        },
-        _ => {
-            let chunks: Vec<&str> = text.split(delimiter).collect();
-            if chunks.len() < read_count {
-                read_count = chunks.len();
-            }
-            chunks[..read_count].join(delimiter)
-        }
-    };
-    println!("{}", chunks)
+    let Expression(count, delimiter) = expr;
+    let result = generate(&text, count, &delimiter);
+    println!("{}", result);
 }
 
 fn help() {
